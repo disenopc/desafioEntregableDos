c76a231d17ec0b4c338239b80c5d42aed14b9a82
disenopc <capitanellipl@gmail.com>
2022-06-09 22:58:16 -0300

    mejore el codigo

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,27 +1,28 @@
 const productoA = [];
 class Producto {
-    constructor(nombre, categoria, precio, stock, img) {
+    constructor(nombre, categoria, precio, stock, img, id) {
         this.nombre = nombre;
-        this.categorio = categoria;
+        this.categoria = categoria;
         this.precio = precio;
         this.stock = stock;
         this.img = img;
+        this.id = id;
 
     }
 
 };
 
-productoA.push(new Producto("Walt-Can Adulto", "Alimento Balanceado", 1000, 0, "./images/waltcanadulto.jpg"))
-productoA.push(new Producto("Dog-Chow Adulto", "Alimento Balanceado", 1000, 0, "./images/dogchowadulto.png"))
-productoA.push(new Producto("Dogui", "Alimento Balanceado", 1000, 0, "./images/dogui.png"))
-productoA.push(new Producto("Econocan", "Alimento Balanceado", 1000, 0, "./images/econocan.png"))
-productoA.push(new Producto("Pedigree Adulto", "Alimento Balanceado", 1000, 0, "./images/pedigreeadulto.png"))
-productoA.push(new Producto("Sabrositos Adulto", "Alimento Balanceado", 1000, 0, "./images/sabrositosadulto.png"))
-productoA.push(new Producto("Jaspe Adulto", "Alimento Balanceado", 1000, 0, "./images/jaspeadulto.png"))
-productoA.push(new Producto("Walt-Can Cachorro", "Alimento Balanceado", 1000, 0, "./images/waltcancachorro.png"))
-productoA.push(new Producto("Jaspe Cachorro", "Alimento Balanceado", 1000, 0, "./images/jaspecachorro.jpg"))
-productoA.push(new Producto("Dog-Chow Cachorro", "Alimento Balanceado", 1000, 0, "./images/dogchowcachorro.png"))
-productoA.push(new Producto("Sabrositos Cachorro", "Alimento Balanceado", 1000, 0, "./images/sabrositoscachorr.png"))
+productoA.push(new Producto("Walt-Can Adulto", "Alimento Balanceado", 1000, 0, "./images/waltcanadulto.jpg", 1))
+productoA.push(new Producto("Dog-Chow Adulto", "Alimento Balanceado", 1000, 0, "./images/dogchowadulto.png", 2))
+productoA.push(new Producto("Dogui", "Alimento Balanceado", 1000, 0, "./images/dogui.png", 3))
+productoA.push(new Producto("Econocan", "Alimento Balanceado", 1000, 0, "./images/econocan.png", 4))
+productoA.push(new Producto("Pedigree Adulto", "Alimento Balanceado", 1000, 0, "./images/pedigreeadulto.png", 5))
+productoA.push(new Producto("Sabrositos Adulto", "Alimento Balanceado", 1000, 0, "./images/sabrositosadulto.png", 6))
+productoA.push(new Producto("Jaspe Adulto", "Alimento Balanceado", 1000, 0, "./images/jaspeadulto.png", 7))
+productoA.push(new Producto("Walt-Can Cachorro", "Alimento Balanceado", 1000, 0, "./images/waltcancachorro.png", 8))
+productoA.push(new Producto("Jaspe Cachorro", "Alimento Balanceado", 1000, 0, "./images/jaspecachorro.jpg", 9))
+productoA.push(new Producto("Dog-Chow Cachorro", "Alimento Balanceado", 1000, 0, "./images/dogchowcachorro.png", 10))
+productoA.push(new Producto("Sabrositos Cachorro", "Alimento Balanceado", 1000, 0, "./images/sabrositoscachorr.png", 11))
 
 const card = document.getElementById("cardDinamica");
 for (let array of productoA) {
@@ -32,7 +33,7 @@ for (let array of productoA) {
             <img class="imgArray card-img-top" src="${array.img}" >
             <h3 class="item-title"> ${array.nombre} </h3>
             <p class="card-text">$${array.precio}</p>
-            <button class="btn btn-warning rounded-pill text-secondary">Agregar al carrito</button>
+            <button id="cardBoton" class="btn btn-warning rounded-pill text-secondary">Agregar al carrito</button>
             </div>
          </div>`
     card.appendChild(productoL);
@@ -60,14 +61,9 @@ fondoFooter.style.background = "black";
 fondoFooter.style.color = "white";
 console.log(fondoMenu.innerHTML);
 
-
-
-
 //Eventos sobre el dom
 
 //Eventos sobre el Formulario Newsletter
-
-
 
 let form = document.getElementById("formulario");
 form.addEventListener("click", (e) => botonEnviar(e));
@@ -117,36 +113,31 @@ function capturarP(e) {
 };
 
 //Eventos de carrito
+let finC = document.getElementById("cardBoton");
+finC.addEventListener("click", (e) => botonCarrito(e));
+const botonCarrito = (e) => {
+    e.preventDefault();
+    let producto = e.target.parentNode.children[5].value;
+    let precio = e.target.parentNode.children[6].value;
+    crearUnCarrito({ producto, precio, id });
+    console.log(carritoDeCompras);
 
-let botonesAgregarAlCarrito = document.querySelectorAll('button');
-botonesAgregarAlCarrito.forEach(el => {
-    el.addEventListener('click', elementosCard)
-});
 
-function elementosCard(event) {
-    const button = event.target;
-    const item = button.closest('.item');
-    const itemTitle = item.querySelector(".item-title").textContent;
-    const itemPrice = item.querySelector(".card-text ").textContent;
-    agregarItemAlCarrito(itemTitle, itemPrice);
+}
+const crearUnCarrito = (producto, precio, id) => {
+    const nuevProd = new Carrito(producto, precio, id);
+    carritoDeCompras.agregarProducto(nuevProd);
 }
 
-const finC = document.getElementsByClassName(".carrito");
+class Carrito {
+    constructor(producto, precio, id) {
+        this.producto = producto;
+        this.precio = precio;
+        this.id = id;
+    }
+    agregarProducto(nuevoElem) {
+        this.carritoDeCompras.push(nuevoElem);
+    }
+}
 
-function agregarItemAlCarrito(itemTitle, itemPrice) {
-    const seleccionDeCarrito = document.createElement("div");
-    const contenidoDelCarrito =
-        ` <div class = "modal" tabindex = "-1" >
-        <div class = "modal-dialog","modal-content","modal-header" >
-        <h4 class = "modal-title">${itemTitle}</h4>
-        <h5 class = "modal-title">${itemPrice}</h5>
-        <button type = "button"class = "btn-close"data - bs - dismiss = "modal"aria - label = "Close"> </button>
-        </div> 
-        <div class = "modal-body">
-        </div>
-        <div class = "modal-footer">
-        <button type = "button"  class = "btn btn-secondary" data - bs - dismiss ="modal"> Cerrar</button> 
-        </div>`
-    seleccionDeCarrito.innerHTML = contenidoDelCarrito;
-    contenedorDeItemsDelCarrito.appendChild(seleccionDeCarrito);
-};
+const carritoDeCompras = new Carrito();
